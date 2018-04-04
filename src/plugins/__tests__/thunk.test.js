6e087a43980925693465bea0f5c7e2a99e01c29d
streamich <streamich@gmail.com>
2018-04-04 23:44:44 +0100

    test: add thunk plugin tests

--- src/plugins/__tests__/thunk.test.js
+++ src/plugins/__tests__/thunk.test.js
@@ -6,8 +6,8 @@ import pluginThunk from '../thunk'
 const createStoreWithPlugin = (reducer = state => state) => {
   return createStore({}, [
     pluginDipatch(),
-    pluginReducer(reducer),
-    pluginThunk()
+    pluginThunk(),
+    pluginReducer(reducer)
   ])
 }
 
@@ -22,7 +22,58 @@ describe('plugin', () => {
     })
 
     it('can pass a function as an action', () => {
-      createStoreWithPlugin()
+      const store = createStoreWithPlugin()
+
+      store.dispatch(() => ({
+        type: 'UNKNOWN'
+      }))
+    })
+
+    it('executes function with correct arguments', () => {
+      const effect = jest.fn()
+
+      effect.mockImplementation(() => ({
+        type: 'UNKNOWN'
+      }))
+
+      const store = createStoreWithPlugin()
+
+      store.dispatch(effect)
+
+      expect(effect).toHaveBeenCalledTimes(1)
+      expect(effect).toHaveBeenCalledWith(store)
+    })
+
+    it('passes though plain actions', () => {
+      const reducer = jest.fn()
+
+      reducer.mockImplementation((state, action) => state)
+
+      const store = createStoreWithPlugin(reducer)
+      const action = {
+        type: 'UNKNOWN'
+      }
+
+      store.dispatch(action)
+
+      expect(reducer).toHaveBeenCalledTimes(1)
+      expect(reducer.mock.calls[0][1]).toBe(action)
+    })
+
+    it('passes to reducer returned actions', () => {
+      const reducer = jest.fn()
+
+      reducer.mockImplementation((state, action) => state)
+
+      const store = createStoreWithPlugin(reducer)
+      const action = {
+        type: 'UNKNOWN'
+      }
+
+      store.dispatch(() => action)
+
+      expect(reducer).toHaveBeenCalledTimes(1)
+      expect(reducer.mock.calls[0][1]).toBe(action)
     })
   })
 })
